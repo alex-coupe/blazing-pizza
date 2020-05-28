db1f7560b82efd870ab7bf26d6dd11e47205e1a7
Alex <acooupe@gmail.com>
2020-05-28 13:23:47 +0100

    JS interop

---
+++ save-points/00-get-started/BlazingPizza.Client/JSRuntimeExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazingPizza.Client
+{
+    public static class JSRuntimeExtensions
+    {
+        public static ValueTask<bool> Confirm(this IJSRuntime jsRuntime, string message)
+        {
+            return jsRuntime.InvokeAsync<bool>("confirm", message);
+        }
+    }
+}
